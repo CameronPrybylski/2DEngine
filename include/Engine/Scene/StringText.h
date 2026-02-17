cb9b9dbe2ad0932bdded348f71e4b811a0c45427
Cameron Przybylski <cameronprzybylski@yahoo.com>
2026-02-17 17:20:10 -0600

    Adding new text system to engine

---
+++ include/Engine/Scene/StringText.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Engine/Scene/LetterText.h>
+
+class StringText : public GameObject 
+{
+public:
+    StringText(std::string letters, glm::vec3 position, glm::vec4 color, std::string fontPath, int fontSize);
+
+    void Render(Renderer& renderer, const Camera& camera) override;
+    
+    virtual void SetTextSizeAndPos();
+
+    void SetLetterTextSizeAndPos(std::shared_ptr<LetterText> letter);
+
+    virtual void ChangeText(std::string newLetters);
+
+    int GetFontSize(){return fontSize;}
+
+    glm::vec3 GetEndPosition(){return endPosition;}
+
+    std::vector<std::shared_ptr<LetterText>> GetLetters(){return letters;}
+
+    std::string GetFontPath(){return fontPath;}
+
+protected:
+    std::vector<std::shared_ptr<LetterText>> letters;
+    int fontSize;
+    std::string fontPath;
+    static std::string lowerLetters;
+    static std::string upperLetters;
+    glm::vec3 endPosition;
+
+};
